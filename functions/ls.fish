354cb160986cbc5fc8e2c719dc2c3d226f7de198
12-G <1240121425@qq.com>
2023-01-21 12:25:15 +0800

    create alias for ls command

---
+++ functions/ls.fish
@@ -0,0 +1,4 @@
+function ls --description 'alias ls ls --color=auto -al --group-directories-first --sort=size -I . -I ..'
+ command ls --color=auto -al --group-directories-first --sort=size -I . -I .. $argv
+        
+end
