1f521ceef3789418b1dff16ee931bc0454df9a83
12-G <1240121425@qq.com>
2023-01-21 12:31:46 +0800

    delete alias 'ls.fish'

--- functions/ls.fish
+++
@@ -1,4 +0,0 @@
-function ls --description 'alias ls ls --color=auto -al --group-directories-first --sort=size -I . -I ..'
- command ls --color=auto -al --group-directories-first --sort=size -I . -I .. $argv
-        
-end
